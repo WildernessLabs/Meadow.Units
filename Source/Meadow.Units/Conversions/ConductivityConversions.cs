31b48121f4d7a984801167330a3952a0838092fc
Adrian Stevens <adrian.stevens@gmail.com>
2023-01-10 14:53:08 -0800

    Add conductivity unit

---
+++ Source/Meadow.Units/Conversions/ConductivityConversions.cs
@@ -0,0 +1,25 @@
+﻿namespace Meadow.Units.Conversions
+{
+    internal static class ConductivityConversions
+    {
+        public static double Convert(double value, Conductivity.UnitType from, Conductivity.UnitType to)
+        {
+            if (from == to)
+            {
+                return value;
+            }
+            return value * conductivityConversions[(int)to] / conductivityConversions[(int)from];
+        }
+
+        //must align to enum
+        private static readonly double[] conductivityConversions =
+        {
+            1, //siemen per cm
+            100, //siemen per m
+			1000, //milli siemen per cm
+            100000, //milli siemen per m
+            1000000, //micro siemen per cm
+            100000000, //micro siemen per m
+		};
+    }
+}
