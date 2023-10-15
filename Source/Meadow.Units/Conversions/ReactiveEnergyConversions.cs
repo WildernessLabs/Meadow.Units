83da879dad2c2232e32c7514c9c653beb93457d4
Adrian Stevens <adrian.stevens@gmail.com>
2023-10-14 22:45:49 -0700

    Add reactive energy unit

---
+++ Source/Meadow.Units/Conversions/ReactiveEnergyConversions.cs
@@ -0,0 +1,24 @@
+﻿namespace Meadow.Units.Conversions
+{
+    internal static class ReactiveEnergyConversions
+    {
+        public static double Convert(double value, ReactiveEnergy.UnitType from, ReactiveEnergy.UnitType to)
+        {
+            if (from == to)
+            {
+                return value;
+            }
+            return value * reactiveEnergyConversions[(int)to] / reactiveEnergyConversions[(int)from];
+        }
+
+        //must align to enum
+        private static readonly double[] reactiveEnergyConversions =
+        {
+            0.000000001,		//Gigavolt amphere
+            0.000001,			//Megavolt amphere
+		    0.001,				//Kilovolt amphere
+		    1.0,				//Volt amphere
+		    1000.0,				//Millivolt amphere
+	    };
+    }
+}
