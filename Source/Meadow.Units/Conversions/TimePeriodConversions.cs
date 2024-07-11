463a487f7c8edc7d054d82f505cb1c989f493609
Adrian Stevens <adrian.stevens@gmail.com>
2024-07-11 15:52:26 -0700

    Release 1.12.8

---
+++ Source/Meadow.Units/Conversions/TimePeriodConversions.cs
@@ -0,0 +1,25 @@
+﻿namespace Meadow.Units.Conversions;
+
+internal static class TimePeriodConversions
+{
+    public static double Convert(double value, TimePeriod.UnitType from, TimePeriod.UnitType to)
+    {
+        if (from == to)
+        {
+            return value;
+        }
+        return value * timeConversions[(int)to] / timeConversions[(int)from];
+    }
+
+    //must align to enum
+    private static readonly double[] timeConversions =
+    {
+        1_000_000_000,//ns
+        1_000_000,//us
+        1_000,//ms
+        1,//s
+        1/60d, //min
+        1/(60d * 60d), //hrs
+        1/(60d * 60d *24d), //days
+    };
+}
--- Source/Meadow.Units/GeoLocation.cs
+++ Source/Meadow.Units/GeoLocation.cs
@@ -1,17 +1,16 @@
-﻿using System;
-using System.Diagnostics.Contracts;
-
-namespace Meadow.Units;
+﻿namespace Meadow.Units;
 
 /// <summary>
 /// Represents a location on the surface of an ideal Earth (latitude and longitude)
 /// </summary>
 public record GeoLocation
 {
+    private static Length? _earthRadius;
+
     /// <summary>
     /// Idealized earth radius used for internal calculations
     /// </summary>
-    public Length EarthRadius => new Length(6371.01, Length.UnitType.Kilometers);
+    public static Length EarthRadius => _earthRadius ??= new Length(6371.01, Length.UnitType.Kilometers);
 
     /// <summary>
     /// The latitude portion of the GeoLocation
@@ -25,90 +24,20 @@ public record GeoLocation
     /// <summary>
     /// Creates a GeoLocation instance
     /// </summary>
-    /// <param name="latitude"></param>
-    /// <param name="longitude"></param>
+    public GeoLocation()
+    {
+        Latitude = 0;
+        Longitude = 0;
+    }
+
+    /// <summary>
+    /// Creates a GeoLocation instance
+    /// </summary>
+    /// <param name="latitude">The latitude portion of the GeoLocation</param>
+    /// <param name="longitude">The longitude portion of the GeoLocation</param>
     public GeoLocation(double latitude, double longitude)
     {
         Latitude = latitude;
         Longitude = longitude;
-    }
-
-    [Pure]
-    private static double DegreesToRadians(double degrees)
-    {
-        return degrees * Math.PI / 180.0;
-    }
-
-    [Pure]
-    private static double RadiansToDegrees(double radians)
-    {
-        return radians * 180 / Math.PI;
-    }
-
-    /// <summary>
-    /// Calculates the distance to another GeoLocation
-    /// </summary>
-    /// <param name="other"></param>
-    /// <returns></returns>
-    [Pure]
-    public Length DistanceTo(GeoLocation other)
-    {
-        var diffLat = DegreesToRadians(other.Latitude - Latitude);
-        var diffLong = DegreesToRadians(other.Longitude - Longitude);
-
-        var a = Math.Sin(diffLat / 2) * Math.Sin(diffLat / 2) +
-                Math.Cos(DegreesToRadians(Latitude)) * Math.Cos(DegreesToRadians(other.Latitude)) *
-                Math.Sin(diffLong / 2) * Math.Sin(diffLong / 2);
-        var c = 2 * Math.Asin(Math.Min(1, Math.Sqrt(a)));
-        var d = EarthRadius * c;
-
-        return d;
-    }
-
-    /// <summary>
-    /// Calculates the bearing to another GeoLocation
-    /// </summary>
-    /// <param name="other"></param>
-    /// <returns></returns>
-    [Pure]
-    public Azimuth BearingTo(GeoLocation other)
-    {
-        var dLon = DegreesToRadians(other.Longitude - Longitude);
-        var dPhi = Math.Log(Math.Tan(DegreesToRadians(other.Latitude) / 2 + Math.PI / 4) / Math.Tan(DegreesToRadians(Latitude) / 2 + Math.PI / 4));
-        if (Math.Abs(dLon) > Math.PI)
-        {
-            dLon = dLon > 0 ? -(2 * Math.PI - dLon) : (2 * Math.PI + dLon);
-        }
-
-        return Azimuth.FromRadians(Math.Atan2(dLon, dPhi));
-    }
-
-    /// <summary>
-    /// Creates a new GeoLocation a given bearing and distance from the current GeoLocation
-    /// </summary>
-    /// <param name="bearing">Bearing angle to the new location</param>
-    /// <param name="distance">Distance to the new location</param>
-    /// <returns></returns>
-    [Pure]
-    public GeoLocation Move(Azimuth bearing, Length distance) // double initialBearingRadians, double distanceKilometres)
-    {
-        var distRatio = distance.Meters / EarthRadius.Meters;
-        var distRatioSine = Math.Sin(distRatio);
-        var distRatioCosine = Math.Cos(distRatio);
-
-        var startLatRad = DegreesToRadians(Latitude);
-        var startLonRad = DegreesToRadians(Longitude);
-
-        var startLatCos = Math.Cos(startLatRad);
-        var startLatSin = Math.Sin(startLatRad);
-
-        var endLatRads = Math.Asin((startLatSin * distRatioCosine) + (startLatCos * distRatioSine * Math.Cos(bearing.Radians)));
-
-        var endLonRads = startLonRad
-                         + Math.Atan2(
-                             Math.Sin(bearing.Radians) * distRatioSine * startLatCos,
-                             distRatioCosine - startLatSin * Math.Sin(endLatRads));
-
-        return new GeoLocation(RadiansToDegrees(endLatRads), RadiansToDegrees(endLonRads));
     }
 }
