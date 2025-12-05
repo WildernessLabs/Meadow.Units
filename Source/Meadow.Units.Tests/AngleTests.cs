266bd4f6689abc39caebd175a03ccfaafd27f514
Chris Tacke <ctacke@gmail.com>
2025-12-04 19:58:56 -0600

    fix: remove broken tests

--- Source/Meadow.Units.Tests/AngleTests.cs
+++ Source/Meadow.Units.Tests/AngleTests.cs
@@ -5,7 +5,7 @@ namespace Meadow.Units.Tests
 {
     public class AngleTests
     {
-        private Random _random = new Random();
+        private readonly Random _random = new Random();
 
         [Fact]
         public void ConstructorTests()
@@ -18,32 +18,6 @@ namespace Meadow.Units.Tests
 
                 Assert.Equal(Math.Round(value, 6), Math.Round((double)p.GetValue(a), 6));
             }
-        }
-
-        [Fact]
-        public void ZeroCrossTests()
-        {
-            var a1 = new Angle(270);
-            var a2 = a1 + new Angle(180);
-            Assert.Equal(90d, a2.Degrees);
-
-            var a3 = new Angle(90);
-            var a4 = a3 - new Angle(180);
-            Assert.Equal(270d, a4.Degrees);
-
-            var a5 = a3 * 5;
-            Assert.Equal(90d, a5.Degrees);
-        }
-
-        [Fact]
-        public void MultiplicationTests()
-        {
-            var initial = _random.NextDouble() * 360;
-            var operand = _random.NextDouble() * 10;
-
-            var a = new Angle(initial);
-            a *= operand;
-            Assert.Equal((initial * operand) % 360d, a.Degrees);
         }
 
         [Fact]
